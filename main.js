4d7e9a7b6bf287fe8861874afdc5cbead5b8c6d4
KasztZ <llfernandollcg@gmail.com>
2022-11-04 17:24:21 -0400

    started adding countries - the hard way

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function createGame(player1, hour, player2) {
     return `
     <li>
-        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+        <img src="./assets/icons/icon-${player1}.svg" alt="Bandeira do ${player1}" class="max-size-52px">
         <strong>${hour}</strong>
-        <img src="./assets/icon-${player2}.svg" alt="Bandeira ${player2}">
+        <img src="./assets/icons/icon-${player2}.svg" alt="Bandeira ${player2}" class="max-size-52px">
     </li>`
 }
 
-let delay = -0.2
+let delay = -0.1
 function createCard(date, day, games) {
-    delay = delay + 0.2;
+    delay = delay + 0.1;
     return `
     <div class="card" style="animation-delay:${delay}s">
         <h2>${date} <span>${day}</span></h2>
@@ -21,7 +21,60 @@ function createCard(date, day, games) {
 
 /* CHAVE: COMUNIDADE - DIA 4*/
 
-document.querySelector("#cards").innerHTML =
-        createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia') + createGame('portugal', '07:00', 'ghana') ) +
-        createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguay') ) +
-        createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameron') )
+document.querySelector("#cards").innerHTML = 
+    createCard(
+        '20/11', 'domingo', 
+            createGame('qatar', '12:00', 'ecuador')
+    ) +
+    createCard(
+        '21/11', 'segunda', 
+            createGame('england', '09:00', 'iran') + 
+            createGame('senegal','12:00','netherlands') + 
+            createGame('united states','15:00','wales')) +
+    createCard(
+        '22/11', 'terça', 
+            createGame('argentina', '06:00', 'saudi arabia') +
+            createGame('denmark', '09:00', 'tunisia') +
+            createGame('mexico', '12:00', 'poland') + 
+            createGame('france', '15:00', 'australia')
+    ) +
+    createCard(
+        '23/11', 'quarta', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '24/11', 'quinta',
+            createGame('', '', '')
+    ) +
+    createCard(
+        '25/11', 'sexta', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '26/11', 'sábado', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '27/11', 'domingo', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '28/11', 'segunda', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '29/11', 'terça', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '30/11', 'quarta', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '01/12', 'quinta', 
+            createGame('', '', '')
+    ) +
+    createCard(
+        '02/12', 'sexta', 
+            createGame('', '', '')
+    )
